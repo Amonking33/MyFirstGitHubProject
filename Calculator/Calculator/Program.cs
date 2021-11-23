e74493a3a53b722d8a5308307168172f016bdca1
Sebastian Mayrhofer <mayrhofer.s1911@gmail.com>
2021-11-23 12:06:18 +0100

    Create basic division calculator

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -8,8 +8,14 @@ namespace Calculator
         {
             Console.WriteLine("** Calculator **");
 
-            
+            Console.Write("Operand 1: ");
+            int op1 = int.Parse(Console.ReadLine());
 
+            Console.Write("Operand 2: ");
+            int op2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{op1} / {op2} = {op1/op2}");
+            
             Console.ReadKey();
         }
     }
